9eb25cbb139ed4453ab6ac0ff263f10388fc2e58
Kazoud <ksdahrouge@gmail.com>
2021-09-24 02:48:02 +0300

    added ray-sphere collision

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -3,8 +3,21 @@
 #include "color.h"
 #include "ray.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+    double a = dot(r.direction(), r.direction());
+    double b = 2.0 * dot(r.direction(), (r.origin() - center));
+    double c = dot(r.origin() - center, r.origin() - center) - radius * radius;
+
+    return (b * b - 4 * a * c > 0);
+}
+
 color ray_color(const ray& r)
 {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    {
+        return color(1, 0, 0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     double t = 0.5 * (unit_direction.y() + 1);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
