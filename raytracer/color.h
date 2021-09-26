346e0c9d5c22dba1ddf1bf1dfb10eebf623bb234
Kazoud <ksdahrouge@gmail.com>
2021-09-26 19:42:23 +0300

    Added multisampling, abstracted the camera class

--- raytracer/color.h
+++ raytracer/color.h
@@ -3,9 +3,19 @@
 #include "vec3.h"
 #include <iostream>
 
-void write_color(std::ostream& out, color pixel_color)
+void write_color(std::ostream& out, color pixel_color, int samples_per_pixel)
 {
-	out << static_cast<int>(255.999 * pixel_color.x()) << ' ' 
-		<< static_cast<int>(255.999 * pixel_color.y()) << ' ' 
-		<< static_cast<int>(255.999 * pixel_color.z()) << std::endl;
+	double r = pixel_color.x();
+	double g = pixel_color.y();
+	double b = pixel_color.z();
+
+	double scale = 1.0 / samples_per_pixel;
+
+	r *= scale;
+	g *= scale;
+	b *= scale;
+
+	out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+		<< static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+		<< static_cast<int>(256 * clamp(b, 0.0, 0.999)) << std::endl;
 }
--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -5,27 +5,28 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 color ray_color(const ray& r, const hittable& world)
 {
     hit_record rec;
 
-    if ( world.hit(r, 0, infinity, rec) )
+    if (world.hit(r, 0, infinity, rec))
     {
-        return 0.5 * (rec.normal + color(1, 1, 1));
+        return 0.5 * (rec.normal + color(1.0, 1.0, 1.0));
     }
     vec3 unit_direction = unit_vector(r.direction());
-    double t = 0.5 * (unit_direction.y() + 1);
+    double t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main() {
 
     // Image
-
     const double aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
 
@@ -35,14 +36,7 @@ int main() {
 
     // Camera
 
-    double viewport_height = 2.0;
-    double viewport_width = aspect_ratio * viewport_height;
-    double focal_length = 1.0;
-
-    point3 origin(0, 0, 0);
-    vec3 horizontal(viewport_width, 0, 0);
-    vec3 vertical(0, viewport_height, 0);
-    point3 lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+    camera cam;
 
     // Render
 
@@ -51,11 +45,16 @@ int main() {
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << std::endl << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            double u = double(i) / (image_width - 1);
-            double v = double(j) / (image_height - 1);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s)
+            {
+                double u = (i + random_double()) / (image_width - 1);
+                double v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
